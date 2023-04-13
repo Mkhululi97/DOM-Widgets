68a171820058025a44c1278ea5adfc0384b6b286
mkhululi <mkhululimtshali@gmail.com>
2023-04-13 16:50:50 +0200

    radio bill widget is complete

--- js/radio-bill.js
+++ js/radio-bill.js
@@ -1,12 +1,48 @@
-// get a reference to the sms or call radio buttons
-
+// get bill item type radio button selected 'call' or 'sms'
+var billItemTypeRadio = document.querySelector(".billItemTypeRadio");
 //get a reference to the add button
+var radioBillAddBtn = document.querySelector(".radioBillAddBtn");
+//get a reference to the call total text
+var callTotalTwo = document.querySelector(".callTotalTwo");
+// get a reference to the sms total text
+var smsTotalTwo = document.querySelector(".smsTotalTwo");
+// get a reference to the total text
+var totalTwo = document.querySelector(".totalTwo");
 
 //create a variable that will keep track of the total bill
+var totatlBill = 0;
+// create a variable that will keep track of the sms total
+var smsTotal = 0;
+// create a variable that will keep track of the calls total
+var callsTotal = 0;
+// reset default text for call sms and total, totals
+callTotalTwo.innerHTML = "0.00";
+smsTotalTwo.innerHTML = "0.00";
+totalTwo.innerHTML = "0.00";
+function radioBillTotal() {
+  // get the value of the selected radio button
+  var checkedRadioBtn = document.querySelector(
+    "input[name='billItemType']:checked"
+  );
+  // update the script totals
+  if (checkedRadioBtn.value === "call") {
+    callsTotal += 2.75;
+  } else if (checkedRadioBtn.value === "sms") {
+    smsTotal += 0.75;
+  }
+  // update the DOM totals
+  callTotalTwo.innerHTML = callsTotal.toFixed(2);
+  smsTotalTwo.innerHTML = smsTotal.toFixed(2);
+  var totalCost = callsTotal + smsTotal;
+  totalTwo.innerHTML = totalCost.toFixed(2);
+  // change the text color based on the criteria
+  if (totalCost >= 50) {
+    // adding the danger class to make the text red
+    totalTwo.classList.add("danger");
+  } else if (totalCost >= 30) {
+    totalTwo.classList.add("warning");
+  }
+}
 
-//add an event listener for when the add button is pressed
-
-//in the event listener get the value from the billItemTypeRadio radio buttons
-// * add the appropriate value to the running total
-// * add nothing for invalid values that is not 'call' or 'sms'.
-// * display the latest total on the screen
+// add an event listener for when the add button is pressed.
+radioBillAddBtn.addEventListener("click", radioBillTotal);
